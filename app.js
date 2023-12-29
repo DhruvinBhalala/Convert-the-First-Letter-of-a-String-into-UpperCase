a003bb129449b8479267321bbf23e593bf36782f
Dhruvin Bhalala <142414337+DhruvinBhalala@users.noreply.github.com>
2023-12-29 16:39:41 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function upperCase() {
+    let string = document.getElementById("string");
+    let results  = capitalizeFirstLetter(string);
+
+    document.getElementById("result").innerHTML = "results";
+}
