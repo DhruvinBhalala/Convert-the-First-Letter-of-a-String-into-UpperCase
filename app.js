21a09b84e07356bed298be73ed52f500f2e29453
Dhruvin Bhalala <142414337+DhruvinBhalala@users.noreply.github.com>
2023-12-29 16:47:40 +0530

    change in js and html

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-function upperCase() {
-    let string = document.getElementById("string");
-    let results  = capitalizeFirstLetter(string);
-
-    document.getElementById("result").innerHTML = "results";
+function capitalize() {
+    const str = document.getElementById("value").value;
+    const words = str.split(" ");
+    const capitalizedWords = words.map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    const capitalizedString = capitalizedWords.join(" ");
+    document.getElementById("result").innerHTML = capitalizedString;
 }
